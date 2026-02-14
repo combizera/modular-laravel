7ea9542a532ceddb3c5990a4920a409ef80e8cc0
combizera <ycombi349@gmail.com>
2026-02-14 10:28:30 -0300

    feat: add tests for Order and Product modules

---
+++ modules/Order/Tests/OrderTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use Modules\Order\Models\Order;
+
+it('should be able to list a order', function () {
+    $order = new Order();
+
+    $this->assertTrue(true);
+});
---
+++ modules/Product/Tests/ProductTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use Modules\Product\Models\Product;
+
+it('should be able to list a product', function () {
+    $product = new Product();
+
+    $this->assertTrue(true);
+});
